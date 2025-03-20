a3e7fdb17ae1adaaa326c8fb3ca7c9ca35c7efd0
shubhu-gitlogger <paygudeshubhankar8120@gmail.com>
2025-03-20 23:59:11 +0530

    Add files via upload

---
+++ project/include/header.php
@@ -0,0 +1,38 @@
+<header id="header">
+	<div id="head" class="parallax" parallax-speed="2">
+		<h1 id="logo" class="text-center">
+			<img class="img-circle" src="assets/images/guy.jpg" alt="">
+			<span class="title">Dr. Rama Bansode</span>
+			<span class="tagline">Professor<br>
+			<span class="tagline">Modern College of Engineering<br>
+				<a href="mailto:rama.bansode@moderncoe.edu.in" target="_blank">rama.bansode@moderncoe.edu.in</a></span>
+		</h1>
+	</div>
+
+	<nav class="navbar navbar-default navbar-sticky">
+		<div class="container-fluid">
+			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+			
+			<div class="navbar-collapse collapse">
+				
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Notes <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="sidebar-left.html">Computer Networking</a></li>
+							<li><a href="sidebar-right.html">Software Testing</a></li>
+							<li><a href="single.html">Discrete Mathematics</a></li>
+						</ul>
+					</li>
+					<li><a href="blog.php">Blog</a></li>
+				</ul>
+			
+			</div><!--/.nav-collapse -->			
+		</div>	
+	</nav>
+</header>
